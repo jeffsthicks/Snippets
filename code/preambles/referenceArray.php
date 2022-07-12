d782e2518840eaaab7cc9b7c543041fe832d776d
Jeff Hicks <jeff.hicks@ed.ac.uk>
2022-07-12 23:48:33 +0100

    new references

--- code/preambles/referenceArray.php
+++ code/preambles/referenceArray.php
@@ -3,7 +3,7 @@
 "ballard2012hochschild"=>"Matthew Ballard and David Favero.
  Hochschild dimensions of tilting objects.
  <em>International Mathematics Research Notices</em>,
-  2012(11):2607-2645, 2012.
+  2012(11):2607--2645, 2012.
 ",
 "#ballard2012hochschild"=>"BF12",
 "craw2007explicit"=>"Alastair Craw.
@@ -23,18 +23,18 @@
 "#hanlon2022aspects"=>"HH22",
 "biran2013lagrangian"=>"Paul Biran and Octav Cornea.
  Lagrangian cobordism. I.
- <em>Journal of the American Mathematical Society</em>, 26(2):295-340,
+ <em>Journal of the American Mathematical Society</em>, 26(2):295--340,
   2013.
 ",
 "#biran2013lagrangian"=>"BC13",
 "seidel2003long"=>"Paul Seidel.
  A long exact sequence for symplectic Floer cohomology.
- <em>Topology</em>, 42:1003-1063, 2003.
+ <em>Topology</em>, 42:1003--1063, 2003.
 ",
 "#seidel2003long"=>"Sei03",
 "polterovich1991surgery"=>"Leonid Polterovich.
  The surgery of Lagrange submanifolds.
- <em>Geometric &amp; Functional Analysis GAFA</em>, 1(2):198-210, 1991.
+ <em>Geometric &amp; Functional Analysis GAFA</em>, 1(2):198--210, 1991.
 ",
 "#polterovich1991surgery"=>"Pol91",
 "perutz2008handleslide"=>"Timothy Perutz.
@@ -43,7 +43,7 @@
 "#perutz2008handleslide"=>"Per08",
 "audin1994symplectic"=>"Mich&egrave;le Audin, Fran&ccedil;ois Lalonde, and Leonid Polterovich.
  Symplectic rigidity: Lagrangian submanifolds.
- In <em>Holomorphic curves in symplectic geometry</em>, pages 271-321.
+ In <em>Holomorphic curves in symplectic geometry</em>, pages 271--321.
   Springer, 1994.
 ",
 "#audin1994symplectic"=>"ALP94",
@@ -55,13 +55,13 @@
 "#oancea2003suite"=>"Oan03",
 "abouzaid2010geometric"=>"Mohammed Abouzaid.
  A geometric criterion for generating the Fukaya category.
- <em>Publications Math&eacute;matiques de l'IH&Eacute;S</em>, 112:191-240,
+ <em>Publications Math&eacute;matiques de l'IH&Eacute;S</em>, 112:191--240,
   2010.
 ",
 "#abouzaid2010geometric"=>"Abo10",
 "viterbo1999functors"=>"Claude Viterbo.
  Functors and computations in Floer homology with applications, i.
- <em>Geometric &amp; Functional Analysis GAFA</em>, 9(5):985-1033, 1999.
+ <em>Geometric &amp; Functional Analysis GAFA</em>, 9(5):985--1033, 1999.
 ",
 "#viterbo1999functors"=>"Vit99",
 "stacks-project"=>"The Stacks project authors.
@@ -76,12 +76,12 @@
 "#wendlbeginner"=>"Wen",
 "seidel2006biased"=>"Paul Seidel.
  A biased view of symplectic cohomology.
- <em>Current developments in mathematics</em>, 2006(1):211-254, 2006.
+ <em>Current developments in mathematics</em>, 2006(1):211--254, 2006.
 ",
 "#seidel2006biased"=>"Sei06",
 "arnol1980lagrange"=>"Vladimir&nbsp;Igorevich Arnol'd.
  Lagrange and Legendre cobordisms. i.
- <em>Funktsional'nyi Analiz i ego Prilozheniya</em>, 14(3):1-13, 1980.
+ <em>Funktsional'nyi Analiz i ego Prilozheniya</em>, 14(3):1--13, 1980.
 ",
 "#arnol1980lagrange"=>"Arn80",
 "fukaya2007lagrangian"=>"K&nbsp;Fukaya, YG&nbsp;Oh, H&nbsp;Ohta, and K&nbsp;Ono.
@@ -91,39 +91,44 @@
   fukaya/Chapter10071117. pdf</em>, 2007.
 ",
 "#fukaya2007lagrangian"=>"FOOO07",
+"biran2014lagrangian"=>"Paul Biran and Octav Cornea.
+ Lagrangian cobordism and fukaya categories.
+ <em>Geometric and functional analysis</em>, 24(6):1731--1830, 2014.
+",
+"#biran2014lagrangian"=>"BC14",
 "bourgeois2009symplectic"=>"Fr&eacute;d&eacute;ric Bourgeois and Alexandru Oancea.
  Symplectic homology, autonomous Hamiltonians, and Morse-Bott
   moduli spaces.
- <em>Duke mathematical journal</em>, 146(1):71-174, 2009.
+ <em>Duke mathematical journal</em>, 146(1):71--174, 2009.
 ",
 "#bourgeois2009symplectic"=>"BO09",
 "auroux2014beginner"=>"Denis Auroux.
  A beginner’s introduction to Fukaya categories.
- In <em>Contact and symplectic topology</em>, pages 85-136. Springer,
+ In <em>Contact and symplectic topology</em>, pages 85--136. Springer,
   2014.
 ",
 "#auroux2014beginner"=>"Aur14",
 "singer1933three"=>"James Singer.
  Three-dimensional manifolds and their Heegaard diagrams.
  <em>Transactions of the American Mathematical Society</em>,
-  35(1):88-111, 1933.
+  35(1):88--111, 1933.
 ",
 "#singer1933three"=>"Sin33",
 "ozsvath2004holomorphic"=>"Peter Ozsv&aacute;th and Zolt&aacute;n Szab&oacute;.
  Holomorphic disks and three-manifold invariants: properties and
   applications.
- <em>Annals of Mathematics</em>, pages 1159-1245, 2004.
+ <em>Annals of Mathematics</em>, pages 1159--1245, 2004.
 ",
 "#ozsvath2004holomorphic"=>"OS04a",
 "ozsvath2004introduction"=>"Peter Ozsv&aacute;th and Zolt&aacute;n Szab&oacute;.
  An introduction to Heegaard Floer homology.
  <em>Floer homology, gauge theory, and low-dimensional topology</em>,
-  5:3-27, 2004.
+  5:3--27, 2004.
 ",
 "#ozsvath2004introduction"=>"OS04b",
 "fet1952variational"=>"Abram&nbsp;Il'ich Fet.
  Variational problems on closed manifolds.
- <em>Matematicheskii Sbornik</em>, 72(2):271-316, 1952.
+ <em>Matematicheskii Sbornik</em>, 72(2):271--316, 1952.
 ",
 "#fet1952variational"=>"Fet52",
 )
